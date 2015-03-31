048d324e21e64a1c8b30b7a120d41fcf0b610f22
David <smrz10@gmail.com>
2015-03-31 14:42:00 +0200

    Limpiamos del fichero del script las funcionalidades ya trasladadas al objeto resize.

--- function.resize.php
+++ function.resize.php
@@ -4,60 +4,6 @@ require 'ImagePath.php';
 require 'Configuration.php';
 require 'Resizer.php';
 
-function isInCache($path, $imagePath) {
-	$isInCache = false;
-	if(file_exists($path) == true):
-		$isInCache = true;
-		$origFileTime = date("YmdHis",filemtime($imagePath));
-		$newFileTime = date("YmdHis",filemtime($path));
-		if($newFileTime < $origFileTime): # Not using $opts['expire-time'] ??
-			$isInCache = false;
-		endif;
-	endif;
-
-	return $isInCache;
-}
-
-function composeNewPath($imagePath, $configuration) {
-	$w = $configuration->obtainWidth();
-	$h = $configuration->obtainHeight();
-	$filename = md5_file($imagePath);
-	$finfo = pathinfo($imagePath);
-	$ext = $finfo['extension'];
-
-	$cropSignal = isset($opts['crop']) && $opts['crop'] == true ? "_cp" : "";
-	$scaleSignal = isset($opts['scale']) && $opts['scale'] == true ? "_sc" : "";
-	$widthSignal = !empty($w) ? '_w'.$w : '';
-	$heightSignal = !empty($h) ? '_h'.$h : '';
-	$extension = '.'.$ext;
-
-	$newPath = $configuration->obtainCache() .$filename.$widthSignal.$heightSignal.$cropSignal.$scaleSignal.$extension;
-
-	if($opts['output-filename']) {
-		$newPath = $opts['output-filename'];
-	}
-
-	return $newPath;
-}
-
-function defaultShellCommand($configuration, $imagePath, $newPath) {
-	$opts = $configuration->asHash();
-	$w = $configuration->obtainWidth();
-	$h = $configuration->obtainHeight();
-
-	$command = $configuration->obtainConvertPath() ." " . escapeshellarg($imagePath) .
-	" -thumbnail ". (!empty($h) ? 'x':'') . $w ."".
-	(isset($opts['maxOnly']) && $opts['maxOnly'] == true ? "\>" : "") .
-	" -quality ". escapeshellarg($opts['quality']) ." ". escapeshellarg($newPath);
-
-	return $command;
-}
-
-function isPanoramic($imagePath) {
-	list($width,$height) = getimagesize($imagePath);
-	return $width > $height;
-}
-
 function composeResizeOptions($imagePath, $configuration) {
 	$opts = $configuration->asHash();
 	$w = $configuration->obtainWidth();
