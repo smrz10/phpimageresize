76fc3ccb00c6fe47819481a1ad02b9405e55c9b5
David <smrz10@gmail.com>
2015-04-05 00:42:19 +0200

    Terminamos de llevar toda la funcionalidad del script a los objetos.
    Se mejora el control de errores para mostrar la descripción, del
    error capturado, que se generará en los procedimientos internos.

--- function.resize.php
+++ function.resize.php
@@ -4,107 +4,26 @@ require 'ImagePath.php';
 require 'Configuration.php';
 require 'Resizer.php';
 
-function composeResizeOptions($imagePath, $configuration) {
-	$opts = $configuration->asHash();
-	$w = $configuration->obtainWidth();
-	$h = $configuration->obtainHeight();
-
-	$resize = "x".$h;
-
-	$hasCrop = (true === $opts['crop']);
-
-	if(!$hasCrop && $configuration->isPanoramic($imagePath)):
-		$resize = $w;
-	endif;
-
-	if($hasCrop && !$configuration->isPanoramic($imagePath)):
-		$resize = $w;
-	endif;
-
-	return $resize;
-}
-
-function commandWithScale($imagePath, $newPath, $configuration) {
-	$opts = $configuration->asHash();
-	$resize = composeResizeOptions($imagePath, $configuration);
-
-	$cmd = $configuration->obtainConvertPath() ." ". escapeshellarg($imagePath) ." -resize ". escapeshellarg($resize) .
-		" -quality ". escapeshellarg($opts['quality']) . " " . escapeshellarg($newPath);
-
-	return $cmd;
-}
-
-function commandWithCrop($imagePath, $newPath, $configuration) {
-	$opts = $configuration->asHash();
-	$w = $configuration->obtainWidth();
-	$h = $configuration->obtainHeight();
-	$resize = composeResizeOptions($imagePath, $configuration);
-
-	$cmd = $configuration->obtainConvertPath() ." ". escapeshellarg($imagePath) ." -resize ". escapeshellarg($resize) .
-		" -size ". escapeshellarg($w ."x". $h) .
-		" xc:". escapeshellarg($opts['canvas-color']) .
-		" +swap -gravity center -composite -quality ". escapeshellarg($opts['quality'])." ".escapeshellarg($newPath);
-
-	return $cmd;
-}
-
-function doResize($resizer, $imagePath, $newPath, $configuration) {
-	$opts = $configuration->asHash();
-	$w = $configuration->obtainWidth();
-	$h = $configuration->obtainHeight();
-
-	if(!empty($w) and !empty($h)):
-		$cmd = commandWithCrop($imagePath, $newPath, $configuration);
-		if(true === $opts['scale']):
-			$cmd = commandWithScale($imagePath, $newPath, $configuration);
-		endif;
-	else:
-		$cmd = $resizer->defaultShellCommand($configuration, $imagePath, $newPath);
-	endif;
-
-	$c = exec($cmd, $output, $return_code);
-	if($return_code != 0) {
-		error_log("Tried to execute : $cmd, return code: $return_code, output: " . print_r($output, true));
-		throw new RuntimeException();
-	}
-}
-
-function resize($imagePath,$opts=null){
-	$path = new ImagePath($imagePath);
-	
+function resize($imagePath,$opts=null){	
 	try {
 	    $configuration = new Configuration($opts);
 	} catch (Exception $e) {
 	    return 'cannot resize the image';
 	}
-	
+
+	$path = new ImagePath($imagePath);	
 	$resizer = new Resizer($path, $configuration);
 
-	// This has to be done in resizer resize
+    //privatizar las funciones que solo se usan desde la clase, crear una llamada __test**** para no rehacer los test existentes	
+    //puedo calcular ObtainFilePath y ComposeNewPath en la construccion de ImagePath? necesitaria configuration
+        //o calcularlos en el constructor de resize y guardarlos como variables privadas
 
 	try {
-		$imagePath = $resizer->obtainFilePath();
+		$cacheFilePath = $resizer->doResize();
 	} catch (Exception $e) {
-		return 'image not found';
+		//return 'cannot resize the image';
+		return $e->getMessage();
 	}
 
-
-	$newPath = $resizer->composeNewPath();
-
-    $create = $resizer->obtainFilePath($newPath, $imagePath);
-
-	if($create == true):
-		try {
-			doResize($resizer, $imagePath, $newPath, $configuration);
-		} catch (Exception $e) {
-			return 'cannot resize the image';
-		}
-	endif;
-
-	// The new path must be the return value of resizer resize
-
-	$cacheFilePath = str_replace($_SERVER['DOCUMENT_ROOT'],'',$newPath);
-
-	return $cacheFilePath;
-	
+	return $cacheFilePath;	
 }
