c3f1f536c6eaaad3c7c7364531464dff96e778fc
David <smrz10@gmail.com>
2015-04-11 14:57:38 +0200

    Se adaptan los test al nuevo constructor con el requerimiento de Cache.

--- test/ImagePathTest.php
+++ test/ImagePathTest.php
@@ -7,23 +7,24 @@ class ImagePathTest extends PHPUnit_Framework_TestCase {
         $url = 'https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#safe=off&q=php%20define%20dictionary';
         $expected = 'https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#safe=off&q=php define dictionary';
 
-        $imagePath = new ImagePath($url);
+        $imagePath = new ImagePath($url, new Cache(30));
 
         $this->assertEquals($expected, $imagePath->sanitizedPath());
     }
 
     public function testIsHttpProtocol() {
         $url = 'https://example.com';
+        $cache = new Cache(30);
 
-        $imagePath = new ImagePath($url);
+        $imagePath = new ImagePath($url,$cache);
 
         $this->assertTrue($imagePath->isHttpProtocol());
 
-        $imagePath = new ImagePath('ftp://example.com');
+        $imagePath = new ImagePath('ftp://example.com',$cache);
 
         $this->assertFalse($imagePath->isHttpProtocol());
 
-        $imagePath = new ImagePath(null);
+        $imagePath = new ImagePath(null,$cache);
 
         $this->assertFalse($imagePath->isHttpProtocol());
     }
@@ -31,7 +32,7 @@ class ImagePathTest extends PHPUnit_Framework_TestCase {
     public function testObtainFileName() {
         $url = 'http://martinfowler.com/mf.jpg?query=hello&s=fowler';
 
-        $imagePath = new ImagePath($url);
+        $imagePath = new ImagePath($url,new Cache(30));
 
         $this->assertEquals('mf.jpg', $imagePath->obtainFileName());
     }
