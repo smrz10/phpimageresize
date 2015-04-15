bccdfac9cdb6aabfd78b20067c41da5d74e86913
David <smrz10@gmail.com>
2015-04-15 14:45:00 +0200

    Agregamos nuevos test, que estaban en ResizerTest. No correspondían.

--- test/ImagePathTest.php
+++ test/ImagePathTest.php
@@ -2,6 +2,20 @@
 require_once 'ImagePath.php';
 
 class ImagePathTest extends PHPUnit_Framework_TestCase {
+    private $requiredArguments = array('h' => 300, 'w' => 600);      
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testNecessaryCollaboration() {
+	$url = 'https://www.google.com/';
+        $imagePath = new ImagePath($url, 'nonCacheObject');
+    }
+
+    public function testInstantiation() {
+	$url = 'https://www.google.com/';
+        $this->assertInstanceOf('ImagePath', new ImagePath($url,new Cache(30)));
+    }
 
     public function testIsSanitizedAtInstantiation() {
         $url = 'https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#safe=off&q=php%20define%20dictionary';
@@ -35,6 +49,62 @@ class ImagePathTest extends PHPUnit_Framework_TestCase {
         $imagePath = new ImagePath($url,new Cache(30));
 
         $this->assertEquals('mf.jpg', $imagePath->obtainFileName());
+    }  
+    
+    /**
+     * @expectedException RuntimeException
+     */
+    public function testObtainFilePathErrorNotFile() {	
+        $configuration = new Configuration($this->requiredArguments);
+        $cache = new Cache($configuration->obtainCacheMinutes());
+        $imagePath = new ImagePath(URL_IMAGE_MF, $cache);                
+        
+        $stub = $this->getMockBuilder('FileSystem')
+            ->getMock();
+        $stub->method('file_exists')
+            ->willReturn(false);                    
+        $cache->injectFileSystem($stub);
+        
+        $imagePath->obtainFilePath($configuration->obtainRemote());            
     }
 
+    public function testObtainLocallyCachedFilePath() {
+        $configuration = new Configuration(array('w' => 800, 'h' => 600));
+        $cache = new Cache($configuration->obtainCacheMinutes());
+        $imagePath = new ImagePath(URL_IMAGE_MF, $cache);
+
+        $stub = $this->obtainMockFileExistsTrue();
+        $stub->method('file_get_contents')
+            ->willReturn('foo');            
+        $cache->injectFileSystem($stub);
+
+        $this->assertEquals('./cache/remote/mf.jpg', $imagePath->obtainFilePath($configuration->obtainRemote()));
+    }
+    
+    public function testLocallyCachedFilePathFail() {
+        $configuration = new Configuration(array('w' => 800, 'h' => 600));
+        $cache = new Cache($configuration->obtainCacheMinutes());
+        $imagePath = new ImagePath(URL_IMAGE_MF, $cache);
+        
+        $stub = $this->obtainMockFileExistsTrue();
+        $stub->method('filemtime')
+            ->willReturn(21 * 60);        
+        $cache->injectFileSystem($stub);
+
+        $this->assertEquals('./cache/remote/mf.jpg', $imagePath->obtainFilePath($configuration->obtainRemote()));
+    }        
+    
+    private function obtainMockFileExistsTrue() {
+	$stubFileSystem = $this->getMockBuilder('FileSystem')
+	    ->getMock();
+	$stubFileSystem->method('file_exists')
+	    ->willReturn(true);
+	$stubFileSystem->method('pathinfo')
+	    ->willReturn(array(
+		'basename' => 'mf.jpg',
+		'extension' => 'jpg'
+	    ));            
+	    
+	return $stubFileSystem;
+    }   
 }
